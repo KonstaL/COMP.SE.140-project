ed470b1fae821bf3073f93aad4295b17c81550ef
Konsta Lehtinen <lehtinen.konsta@gmail.com>
2020-12-04 22:45:02 +0200

    Add camelCase tests

---
+++ tests/camel-case.test.js
@@ -0,0 +1,17 @@
+import camelCase from "../comp.se.200-lib/src/camelCase.js";
+
+describe('at function', () => {
+    test('CamelCase function should convert string to camel case', () => {
+        const a = 'camel case';
+        const expectedResult = "camelCase";
+        
+        expect(camelCase(a)).toBe(expectedResult);
+    });
+
+    test('CamelCase function should return string as it was', () => {
+        const a = 'string';
+        const expectedResult = "string";
+        
+        expect(camelCase(a)).toBe(expectedResult);
+    });
+});
