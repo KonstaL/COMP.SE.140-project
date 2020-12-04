18a610d9e9b082b6850e358bcb64244050d9a557
Konsta Lehtinen <lehtinen.konsta@gmail.com>
2020-12-04 23:03:49 +0200

    Add cast array tests

---
+++ tests/cast-array.test.js
@@ -0,0 +1,20 @@
+import castArray from "../comp.se.200-lib/src/castArray.js";
+
+describe('castArray function', () => {
+    test('castArray should return new array', () => {
+        const a = 'string';
+        const expectedResult = ['string'];
+        expect(castArray()).toEqual(expectedResult);
+    });
+
+    test('castArray should return empty array', () => {
+        const expectedResult = [];
+        expect(castArray()).toEqual(expectedResult);
+    });
+
+    test('castArray should return array', () => {
+        const a = [1,2,3];
+        const expectedResult = [1,2,3];
+        expect(castArray(a)).toEqual(expectedResult);
+    });
+});
