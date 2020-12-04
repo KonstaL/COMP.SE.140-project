35b37bec9cca5b5135764e38d87a97fc5954f7d7
Vilho Aaltonen <vilho.aaltonen@gmail.com>
2020-12-04 23:23:14 +0200

    Add words tests

---
+++ tests/words.test.js
@@ -0,0 +1,26 @@
+import words from '../comp.se.200-lib/src/words';
+
+describe("words function", () => {
+    test("Words with simple input string", () => {
+        const string = 'this is a string';
+        const expectedResult = ['this', 'is', 'a', 'string'];
+        const result = words(string);
+        expect(result).toEqual(expect.arrayContaining(expectedResult));
+    })
+
+    test("Words with simple input string and defined pattern", () => {
+        const string = 'this is & a string';
+        const expectedResult = ['this', 'is', '&', 'a', 'string'];
+        const pattern = /[^,]+/g
+        const result = words(string, pattern);
+        expect(result).toEqual(expect.arrayContaining(expectedResult));
+    })
+
+    test("Words with input that has ascii characters", () => {
+        const string = 'this is an ascii %';
+        const expectedResult = ['this', 'is', 'an', 'ascii'];
+        const result = words(string);
+        expect(result).toEqual(expect.arrayContaining(expectedResult));
+        
+    })
+})
