d4528bb82e18608bbc3096847a2a3b5bd9d4cda0
Konsta Lehtinen <lehtinen.konsta@gmail.com>
2020-12-06 14:04:19 +0200

    Add difference testing

---
+++ tests/difference.test.js
@@ -0,0 +1,30 @@
+import difference from "../comp.se.200-lib/src/difference.js";
+
+describe('difference fn', () => {
+    test('difference should return array size of one when only there is one value difference', () => {
+        const a = [1,2,3];
+        const b = [1,2,4];
+        const expectedResult = 4;
+        
+        expect(difference(a,b)).toEqual(expectedResult);
+    });
+
+    test('difference should return array size of one when only there is one value difference', () => {
+        const a = [1,2,3];
+        const b = [1];
+        const c = [2];
+        const d = [3];
+        const expectedResult = [];
+        
+        expect(difference(a,b,c,d)).toEqual([expectedResult]);
+    });
+
+
+    test('difference null handling', () => {
+        const a = null;
+        const b = [1,2,3];
+        const expectedResult = [];
+        
+        expect(difference(a, b)).toEqual([expectedResult]);
+    });
+});
