a350ab1523f336a98a61d2e0b8069806c8c511ee
Vilho Aaltonen <vilho.aaltonen@gmail.com>
2020-12-06 16:17:31 +0200

    Add tests

---
+++ tests/endsWith.test.js
@@ -0,0 +1,30 @@
+import endsWith from '../comp.se.200-lib/src/endsWith';
+
+describe("endsWith function", () => {
+    test("endsWith should return true when called with string and letter it ends to", () => {
+        const s = 'string';
+        const l = 'g';
+        const result = endsWith(s, l);
+        expect(result).toBe(true)
+    })
+
+
+    test("endsWith should return true when called with string and letter from position", () => {
+        const s = 'string';
+        const l = 'r';
+        const n = 3
+        const result = endsWith(s, l, n);
+        
+        expect(result).toBe(true)
+    })
+
+    test("endsWith should return false when called with string and wrong letter from position", () => {
+        const s = 'string';
+        const l = 'i';
+        const n = 2
+        const result = endsWith(s, l, n);
+        
+        expect(result).toBe(false)
+    })
+
+})
---
+++ tests/eq.test.js
@@ -0,0 +1,21 @@
+import eq from '../comp.se.200-lib/src/eq';
+
+describe("eq function", () => {
+    test("eq should return true when comparing same value", () => {
+        const a = {'a': 1};
+        const result = eq(a,a);
+        expect(result).toBe(true);
+    })
+
+    test("eq should return false when comparing objects that are not the same instance", () => {
+        const a = {'a': 1};
+        const b = {'a': 1};
+        const result = eq(a,b);
+        expect(result).toBe(false);
+    })
+    test("eq should return true when comparing two NaNs", () => {
+        const result = eq(NaN, NaN);
+        expect(result).toBe(true);
+    })
+
+})
