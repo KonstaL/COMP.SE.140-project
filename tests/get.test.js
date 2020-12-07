d7dc6825ace10018ea08869387e16e3946724ab5
Vilho Aaltonen <vilho.aaltonen@gmail.com>
2020-12-07 07:00:05 +0200

    Fix typos

--- tests/get.test.js
+++ tests/get.test.js
@@ -1,40 +1,37 @@
-import get from '../comp.se.200-lib/src/get';
+import get from "../comp.se.200-lib/src/get";
 
-describe("test function", () => {
-    test("get should return correct value when called with valid arguments", () => {
-        const object = {'a': {'b': {'c':1}}}
-        const path = 'a.b.c';
-        const result = get(object, path);
-        const expectedResult = 1;
-        expect(result).toEqual(expectedResult)
-    })
+describe("get function", () => {
+  test("get should return correct value when called with valid arguments", () => {
+    const object = { a: { b: { c: 1 } } };
+    const path = "a.b.c";
+    const result = get(object, path);
+    const expectedResult = 1;
+    expect(result).toEqual(expectedResult);
+  });
 
-    test("get should return default value when called with invalid path and setted defaultvalue", () => {
-        const object = {'a': {'b': {'c':1}}}
-        const path = 'a[2].b.c';
-        const defaultValue = 'def'
-        const result = get(object, path, defaultValue);
-        const expectedResult = defaultValue;
-        expect(result).toEqual(expectedResult)
-    })
+  test("get should return default value when called with invalid path and setted defaultvalue", () => {
+    const object = { a: { b: { c: 1 } } };
+    const path = "a[2].b.c";
+    const defaultValue = "def";
+    const result = get(object, path, defaultValue);
+    const expectedResult = defaultValue;
+    expect(result).toEqual(expectedResult);
+  });
 
-    test("get should return correct value when path is array", () => {
-        const object = {'a': {'b': {'c':1}}}
-        const path = ['a', '0', 'b', 'c']
-        const result = get(object, path);
-        const expectedResult = 1;
-        expect(result).toEqual(expectedResult)
-    })
+  test("get should return correct value when path is array", () => {
+    const object = { a: { b: { c: 1 } } };
+    const path = ["a", "0", "b", "c"];
+    const result = get(object, path);
+    const expectedResult = 1;
+    expect(result).toEqual(expectedResult);
+  });
 
-    test("get should return default value when called with null  setted defaultvalue", () => {
-        const object = null
-        const path = 'a[2].b.c';
-        const defaultValue = 'def'
-        const result = get(object, path, defaultValue);
-        const expectedResult = defaultValue;
-        expect(result).toEqual(expectedResult)
-    })
-
-
-
-})
+  test("get should return default value when called with null  setted defaultvalue", () => {
+    const object = null;
+    const path = "a[2].b.c";
+    const defaultValue = "def";
+    const result = get(object, path, defaultValue);
+    const expectedResult = defaultValue;
+    expect(result).toEqual(expectedResult);
+  });
+});
--- tests/isArguments.test.js
+++ tests/isArguments.test.js
@@ -1,16 +1,15 @@
-import isArguments from '../comp.se.200-lib/src/isArguments';
+import isArguments from "../comp.se.200-lib/src/isArguments";
 
 describe("isArguments function", () => {
-    
-    test("isArguments should return true when input is a function", () => {
-        const a = () => 1;
-        const result = isArguments(a);
-        expect(result).toBe(true);
-    })                
-    
-    test("isArguments should return false when input is a fun", () => {
-        const a = [1, 2, 3];
-        const result = isArguments(a);
-        expect(result).toBe(false);
-    })        
-})
+  test("isArguments should return true when input is a function", () => {
+    const a = () => 1;
+    const result = isArguments(a);
+    expect(result).toBe(true);
+  });
+
+  test("isArguments should return false when input is an array", () => {
+    const a = [1, 2, 3];
+    const result = isArguments(a);
+    expect(result).toBe(false);
+  });
+});
